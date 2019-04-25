c0356fb3bd257aa60d36211c899c417794383457
Sinh Ngo <kaka.ngo@gmail.com>
2019-04-25 09:57:33 +0700

    add real time update

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,20 @@ import React, { Component } from "react";
 import "./App.css";
 import HeadTitle from "./HeadTitle";
 import Member from "./Member";
+import Guest from "./Guest";
 import { db } from "../firebaseConnect";
 
 class App extends Component {
   state = {
     memberData: [],
-    guestData: []
+    guestData: [],
+    showMember: true,
+    showGuest: false
   };
 
-  printData = () => {
+  printMember = () => {
     if (this.state.memberData !== null) {
-      return this.state.memberData.map((value, key) => {
+      return Object.values(this.state.memberData).map((value, key) => {
         return (
           <Member
             key={value.RFID}
@@ -29,26 +32,103 @@ class App extends Component {
     }
   };
 
+  printGuest = () => {
+    if (this.state.guestData !== null) {
+      return Object.values(this.state.guestData).map((value, key) => {
+        return (
+          <Guest
+            key={value.RFID}
+            stt={key + 1}
+            rfid={value.RFID}
+            lastActivity={value.lastActivity}
+            isCurrentlyIn={value.isCurrentlyIn}
+          />
+        );
+      });
+    }
+  };
+
   async componentDidMount() {
     const memberData = [];
     const guestData = [];
 
+    // First time fetching
     await db
       .collection("members")
       .get()
       .then(querySnapshot => {
-        querySnapshot.forEach(c => memberData.push(c.data()));
+        querySnapshot.forEach(
+          member => (memberData[member.data().RFID] = member.data())
+        );
       });
     this.setState({ memberData });
 
+    // Listening for changes only
+    await db.collection("members").onSnapshot(qs => {
+      qs.docChanges().forEach(change => {
+        const data = change.doc.data();
+
+        this.setState({ memberData: { ...memberData, [data.RFID]: data } });
+      });
+    });
+
+    // First time fetching
     await db
       .collection("guests")
       .get()
       .then(querySnapshot => {
-        querySnapshot.forEach(g => guestData.push(g.data()));
+        querySnapshot.forEach(
+          guest => (guestData[guest.data().RFID] = guest.data())
+        );
       });
     this.setState({ guestData });
+
+    // Listening for changes only
+    await db.collection("guests").onSnapshot(qs => {
+      qs.docChanges().forEach(change => {
+        const data = change.doc.data();
+
+        this.setState({ guestData: { ...guestData, [data.RFID]: data } });
+      });
+    });
   }
+
+  renderMember = () => {
+    if (this.state.showMember === true)
+      return (
+        <table className="table table-striped table-hover">
+          <thead className="thead-inverse">
+            <tr>
+              <th>STT</th>
+              <th>RFID</th>
+              <th>Họ tên</th>
+              <th>Lớp</th>
+              <th>Bển số xe</th>
+              <th>Trạng thái</th>
+              <th>Hoạt động cuối</th>
+            </tr>
+          </thead>
+          <tbody>{this.printMember()}</tbody>
+        </table>
+      );
+  };
+
+  renderGuest = () => {
+    if (this.state.showGuest === true)
+      return (
+        <table className="table table-striped table-hover">
+          <thead className="thead-inverse">
+            <tr>
+              <th>STT</th>
+              <th>RFID</th>
+              <th>Trạng thái</th>
+              <th>Hoạt động cuối</th>
+            </tr>
+          </thead>
+          <tbody>{this.printGuest()}</tbody>
+        </table>
+      );
+  };
 
   render() {
     return (
@@ -57,20 +137,28 @@ class App extends Component {
         <hr />
         <div className="container">
           <div className="row">
-            <table className="table table-striped table-hover">
-              <thead className="thead-inverse">
-                <tr>
-                  <th>STT</th>
-                  <th>RFID</th>
-                  <th>Họ tên</th>
-                  <th>Lớp</th>
-                  <th>Bển số xe</th>
-                  <th>Trạng thái</th>
-                  <th>Hoạt động cuối</th>
-                </tr>
-              </thead>
-              <tbody>{this.printData()}</tbody>
-            </table>
+            <div className="btn-group">
+              <button
+                onClick={() => {
+                  this.setState({ showMember: true, showGuest: false });
+                }}
+                type="button"
+                className="btn btn-primary"
+              >
+                Member
+              </button>
+              <button
+                onClick={() => {
+                  this.setState({ showMember: false, showGuest: true });
+                }}
+                type="button"
+                className="btn btn-primary"
+              >
+                Guest
+              </button>
+            </div>
+            {this.renderMember()}
+            {this.renderGuest()}
           </div>
         </div>
       </div>
--- src/components/Guest.js
+++ src/components/Guest.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class Member extends Component {
+export default class Guest extends Component {
   showTrangThai = () => {
     console.log(this.props.isCurrentlyIn);
     if (this.props.isCurrentlyIn) return <i className="fas fa-check-square" />;
@@ -12,9 +12,6 @@ export default class Member extends Component {
       <tr>
         <td>{this.props.stt}</td>
         <td>{this.props.rfid}</td>
-        <td>{this.props.name}</td>
-        <td>{this.props.class}</td>
-        <td>{this.props.plate}</td>
         <td className=" text-center">{this.showTrangThai()}</td>
         <td>{new Date(this.props.lastActivity.toDate()).toString()}</td>
       </tr>
